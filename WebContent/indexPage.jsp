29fed822dcf139623bce86286ae772aa6d1605dc
Daniel Nikulin <danil.nikulin@gmal.com>
2017-11-18 12:09:44 +0200

    Init
    Initializate

---
+++ WebContent/indexPage.jsp
@@ -0,0 +1,27 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%--
+  Created by IntelliJ IDEA.
+  User: Daniel Nikulin/Nikita Khomenko
+  Date: 2017-11-18
+  To change this template use File | Settings | File Templates.
+--%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Title</title>
+<link rel="stylesheet" href="<c:url value="/static/css/stars.css"/>"
+	type="text/css">
+<link rel="stylesheet" href="<c:url value="/static/css/formval.css"/>"
+	type="text/css">
+<link href="static/css/bootstrap.min.css" rel="stylesheet">
+<link href="static/css/starter-template.css" rel="stylesheet">
+</head>
+<body>
+	
+	Hello wrld!
+</body>
+</html>
---
+++ src/main/java/IndexPageServlet.java
@@ -0,0 +1,36 @@
+package main.java;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class IndexPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public IndexPageServlet() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		RequestDispatcher dispatcher = (RequestDispatcher) request.getRequestDispatcher("/indexPage.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

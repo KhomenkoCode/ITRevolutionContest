7a9541813983247cc91f4d2a9010b6c36635703d
Mykyta <bri020697@gmail.com>
2017-11-19 11:10:40 +0200

    +imports

--- src/main/java/MainPageServlet.java
+++ src/main/java/MainPageServlet.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
+import main.java.RatingDAO;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;

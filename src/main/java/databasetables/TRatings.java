806ada3e115022085f3650baba7977bfdb42a3a8
Mykyta <bri020697@gmail.com>
2017-11-19 02:40:57 +0200

    push

--- src/main/java/databasetables/TRatings.java
+++ src/main/java/databasetables/TRatings.java
@@ -1,13 +1,30 @@
 package main.java.databasetables;
 
-public class TRatings {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "tratings")
+public class TRatings implements Serializable {
 	private int rating;
-	private String artist_id;
-	private String user_id;
 	private String rating_id;
 	
 	
 	private TUsers user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
 	public TUsers getUser() {
 		return user;
 	}
@@ -15,36 +32,32 @@ public class TRatings {
 	public void setUser(TUsers user) {
 		this.user = user;
 	}
-	private TArtists artist;
 	
+	private TArtists artist;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "artist_id", nullable = false)
 	public TArtists getArtist() {
 		return artist;
 	}
-
+	
 	public void setArtist(TArtists artist) {
 		this.artist = artist;
 	}
 
 	public TRatings(){}
 	
+	@Column(name = "rating")
 	public int getRating() {
 		return rating;
 	}
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-	public String getArtist_id() {
-		return artist_id;
-	}
-	public void setArtist_id(String artist_id) {
-		this.artist_id = artist_id;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
+	
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "rating_id", unique = true, nullable = false)
 	public String getRating_id() {
 		return rating_id;
 	}
